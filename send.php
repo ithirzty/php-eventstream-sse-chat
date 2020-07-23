36689bc46bc1b5bf736ca93d092ef7dcdf24d3dd
ithirzty <45016834+ithirzty@users.noreply.github.com>
2020-07-23 22:34:19 +0200

    Update send.php
    Fixed message bug and added custom vars

--- send.php
+++ send.php
@@ -2,20 +2,21 @@
 session_start();
 
 //VARS TO SET
-$username = 'SENDER\'S USERNAME';
+$username = 'usrename'; //sender's username
+$time_between_message = 1; //time to pass between two message send by the same user
+$message_max_length = 200; //max length of a message
 
 
-
-if(strlen($_GET['msg']) <= 200) {
-$msg = [$username => htmlspecialchars($_GET['msg'])];
-  if(time() - $_SESSION['lastmsginchat'] >= 1) {
+if($_GET['message'] != '' && strlen($_GET['msg']) <= $message_max_length) {
+$message = [$username => htmlspecialchars($_GET['msg'])];
+  if(time() - $_SESSION['lastmsginchat'] >= $time_between_message) {
     $msgs = apc_fetch('chat');
     foreach($msgs as $time => $msg) {
       if((microtime(true) - $time) >= 5) {
       unset($msgs[$time]);
       }
     }
-    $msgs[''.microtime(true).''] = $msg;
+    $msgs[''.microtime(true).''] = $message;
     apc_store('chat', $msgs);
 }
 $_SESSION['lastmsginchat'] = time();
